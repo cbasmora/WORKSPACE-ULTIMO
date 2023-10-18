1979c2ee1c96e6dbd220f34209bf40b3228cfee0
Cbasmora <ing.sebasmora@gmail.com>
2023-10-17 21:28:22 -0500

    CON BUSCADOR FUNCIONANDO

--- script.js
+++ script.js
@@ -1,53 +1,19 @@
 const searchInput = document.getElementById("search");
-const content = document.getElementById("content");
+const documentList = document.getElementById("document-list");
 
-searchInput.addEventListener("input", highlightSearch);
+searchInput.addEventListener("input", filterDocuments);
 
-function highlightSearch() {
-    const searchTerm = searchInput.value.trim().toLowerCase();
-    if (!searchTerm) {
-        removeHighlights(content);
-        return;
-    }
+function filterDocuments() {
+    const searchTerm = searchInput.value.toLowerCase();
+    const items = documentList.getElementsByTagName("li");
 
-    const textNodes = getTextNodes(content);
-
-    textNodes.forEach((node) => {
-        const text = node.textContent.toLowerCase();
-        const index = text.indexOf(searchTerm);
-        if (index >= 0) {
-            const before = document.createTextNode(text.substring(0, index));
-            const match = document.createElement("span");
-            match.textContent = text.substring(index, index + searchTerm.length);
-            match.classList.add("highlight");
-            const after = document.createTextNode(text.substring(index + searchTerm.length));
-
-            const parent = node.parentNode;
-            parent.replaceChild(after, node);
-            parent.replaceChild(match, after);
-            parent.replaceChild(before, match);
-        }
-    });
-}
-
-function getTextNodes(node) {
-    const textNodes = [];
-    if (node.nodeType === Node.TEXT_NODE) {
-        textNodes.push(node);
-    } else {
-        const children = node.childNodes;
-        for (let i = 0; i < children.length; i++) {
-            textNodes.push(...getTextNodes(children[i]));
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        const documentName = item.textContent.toLowerCase();
+        if (documentName.includes(searchTerm)) {
+            item.style.display = "list-item";
+        } else {
+            item.style.display = "none";
         }
     }
-    return textNodes;
-}
-
-function removeHighlights(node) {
-    const highlights = node.querySelectorAll(".highlight");
-    highlights.forEach((highlight) => {
-        const parent = highlight.parentNode;
-        const text = document.createTextNode(highlight.textContent);
-        parent.replaceChild(text, highlight);
-    });
 }
